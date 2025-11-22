31ba313b2411fc061297f5dcaf1ade23730e6f3e
kobe wijnants <wijnantskobe28@gmail.com>
2025-11-22 16:19:31 +0100

    remove argocd

--- provider.tf
+++ provider.tf
@@ -23,17 +23,3 @@ variable "proxmox_password" {
   sensitive   = true
   description = "Password for your proxmox node"
 }
-
-locals {
-  kube_config = yamldecode(module.talos.kubeconfig)
-}
-
-provider "helm" {
-  kubernetes = {
-    host                   = local.kube_config.clusters[0].cluster.server
-    
-    client_certificate     = base64decode(local.kube_config.users[0].user.client-certificate-data)
-    client_key             = base64decode(local.kube_config.users[0].user.client-key-data)
-    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
-  }
-}
